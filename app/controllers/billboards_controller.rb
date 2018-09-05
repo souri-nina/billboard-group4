c7ef72b6fc5989218074f5b77d262bcc2c247cde
Mike Coker <mbcoker100@gmail.com>
2018-09-05 16:08:39 -0600

    added billboard controller

--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -1,13 +1,54 @@
 class BillboardsController < ApplicationController
+  before_action :set_billboard, only: [:show, :update, :edit, :destroy]
+
+  
   def index
+    @billboards = Billboard.all
   end
 
   def show
+
+
   end
 
   def edit
   end
 
   def new
+    @billboard = Billboard.new
   end
+
+  def create
+    @billboard = Billoard.new(billboard_params)
+    
+    if @billboard.save
+      redirect_to billboards_path
+    else
+      render :new
+    end
+  end
+
+
+  def update
+    if @billboard.update(billboard_params)
+      redirect_to @billboard
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @billboard.destroy
+    redirect_to billboards_path
+  end
+
+  private
+     def set_billboard
+      @billboard = Billboard.find(params[:id])
+    end
+
+    def billboard_params
+      params.require(:billboard).permit(:title :artist_name :song)
+    end
+
 end
