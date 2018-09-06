2365b10a2fa91a7f2c5d283fba9097926b7dff7f
parkerprecisionweb <dallasjamesparker@gmail.com>
2018-09-06 16:44:07 -0600

    css framwork/seed file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |i|
+  billboard = Billboard.create(title: "Billboard_Top_#{i}")
+
+  5.times do |j|
+    billboard.artists.create(
+      artist_name: "name_#{i}_#{j}",
+      song_name: "song_#{i}_#{j}"
+      )
+  end
+end
